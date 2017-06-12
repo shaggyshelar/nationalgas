a26f2d58bb42e32c9dadf0c7fded959889a85521
ManishPatil159 <manish.patil159@gmail.com>
2017-06-12 15:22:22 +0530

    Added missing methods in CustomerController

---
+++ NG.Service/Controllers/Customers/CustomerRegistrationReportDto.cs
@@ -0,0 +1,11 @@
+namespace NG.Service.Controllers.Customers
+{
+    public class CustomerRegistrationReportDto
+    {
+        public int CustomerCount { get; set; }
+        public int MaleCount { get; set; }
+        public int FemaleCount { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+    }
+}
